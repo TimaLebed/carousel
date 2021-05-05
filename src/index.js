659d4d13ff6b4203dbcee11e34c11e880591fa94
TimaLebed <timalebedevskiy@mail.ru>
2021-05-05 18:29:19 +0300

    feat: add swipe with the help of click

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ function hideItem(direction) {
     this.classList.remove('active', direction);
   });
 }
+
 function showItem(direction) {
   items[currentItem].classList.add('next', direction);
   items[currentItem].addEventListener('animationend', function () {
@@ -25,19 +26,70 @@ function showItem(direction) {
     isEnabled = true;
   });
 }
+
 function previousItem(n) {
   hideItem('to-right');
   changeCurrentItem(n - 1);
   showItem('from-left');
 }
+
 function nextItem(n) {
   hideItem('to-left');
   changeCurrentItem(n + 1);
   showItem('from-right');
 }
+
 controlLeft.addEventListener('click', () => {
   if (isEnabled) previousItem(currentItem);
 });
+
 controlRight.addEventListener('click', () => {
   if (isEnabled) nextItem(currentItem);
 });
+
+function swipe(item) {
+  const surface = item;
+  let startX = 0;
+  let startY = 0;
+  let distX = 0;
+  let distY = 0;
+
+  let startTime = 0;
+  let elapsedTime = 0;
+
+  const thresholdX = 150;
+  const thresholdY = 75;
+  const allowedTime = 300;
+
+  surface.addEventListener('mousedown', (e) => {
+    startX = e.pageX;
+    startY = e.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  });
+
+  surface.addEventListener('mouseup', (e) => {
+    distX = e.pageX - startX;
+    distY = e.pageY - startY;
+    elapsedTime = new Date().getTime() - startTime;
+
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) > thresholdX && Math.abs(distY) < thresholdY) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousItem(currentItem);
+          }
+        } else if (isEnabled) {
+          nextItem(currentItem);
+        }
+      }
+    }
+
+    e.preventDefault();
+    console.log(distX, distY, elapsedTime);
+  });
+}
+
+const el = document.querySelector('.item-container');
+
+swipe(el);
